c19da03c77e7d63926806a85f14af2634c5a6a76
Gary.nG <gary.gng@me.com>
2016-02-21 17:38:32 +0800

    Initial Commit

---
+++ L02_TouchID/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  L02_TouchID
+//
+//  Created by Gary.nG on 2/21/16.
+//  Copyright © 2016 Gary.nG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
